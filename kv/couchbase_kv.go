288f96b5b24611a17fcf45408b268d49f3f92d4a
Vlad Emelianov <vlad@movio.co>
2017-02-13 11:33:28 +1300

    comments for kv stores

--- kv/couchbase_kv.go
+++ kv/couchbase_kv.go
@@ -6,12 +6,15 @@ import (
 	couch "gopkg.in/couchbaselabs/gocb.v1"
 )
 
+// CouchbaseKeyValueStore is a key-value storage that uses Couchbase Data Storage.
+// See: http://docs.couchbase.com/admin/admin/Concepts/concept-dataStorage.html
 type CouchbaseKeyValueStore struct {
 	cluster *couch.Cluster
 	bucket  *couch.Bucket
 	config  *CouchbaseConfig
 }
 
+// CouchbaseConfig describes a config for store
 type CouchbaseConfig struct {
 	Host          string
 	Bucket        string
@@ -21,6 +24,7 @@ type CouchbaseConfig struct {
 	ReplicateTo   uint
 }
 
+// NewCouchbaseKeyValueStore creates new store connection
 func NewCouchbaseKeyValueStore(config *CouchbaseConfig) (*CouchbaseKeyValueStore, error) {
 	cluster, err := couch.Connect(fmt.Sprintf("couchbase://%s", config.Host))
 	if err != nil {
@@ -37,6 +41,7 @@ func NewCouchbaseKeyValueStore(config *CouchbaseConfig) (*CouchbaseKeyValueStore
 	}, nil
 }
 
+// Get gets data by key from store and populates value
 func (s *CouchbaseKeyValueStore) Get(key string, value StoreValue) (bool, error) {
 	_, err := s.bucket.Get(key, value)
 	if err == couch.ErrKeyNotFound {
@@ -48,6 +53,7 @@ func (s *CouchbaseKeyValueStore) Get(key string, value StoreValue) (bool, error)
 	return true, nil
 }
 
+// Put updates key in store with serialized value
 func (s *CouchbaseKeyValueStore) Put(key string, value StoreValue) error {
 	if s.config.DurableWrites {
 		_, err := s.bucket.UpsertDura(key, value, 0, s.config.ReplicateTo, s.config.PersistTo)
@@ -58,6 +64,7 @@ func (s *CouchbaseKeyValueStore) Put(key string, value StoreValue) error {
 	return err
 }
 
+// Delete removes key from store
 func (s *CouchbaseKeyValueStore) Delete(key string) error {
 	if s.config.DurableWrites {
 		_, err := s.bucket.RemoveDura(key, 0, s.config.ReplicateTo, s.config.PersistTo)
--- kv/in_memory_kv.go
+++ kv/in_memory_kv.go
@@ -5,16 +5,19 @@ import (
 	"reflect"
 )
 
+// InMemoryKeyValueStore is a key-value storage that stores data in memory using map
 type InMemoryKeyValueStore struct {
 	m map[string]StoreValue
 }
 
+// NewInMemoryKeyValueStore creates new store
 func NewInMemoryKeyValueStore(size int) *InMemoryKeyValueStore {
 	return &InMemoryKeyValueStore{
 		m: make(map[string]StoreValue, size),
 	}
 }
 
+// Get gets data by key from store and populates value
 func (s *InMemoryKeyValueStore) Get(key string, dst StoreValue) (bool, error) {
 	src, found := s.m[key]
 	if !found {
@@ -32,11 +35,13 @@ func (s *InMemoryKeyValueStore) Get(key string, dst StoreValue) (bool, error) {
 	return true, nil
 }
 
+// Put updates key in store with serialized value
 func (s *InMemoryKeyValueStore) Put(key string, value StoreValue) error {
 	s.m[key] = value
 	return nil
 }
 
+// Delete removes key from store
 func (s *InMemoryKeyValueStore) Delete(key string) error {
 	delete(s.m, key)
 	return nil
