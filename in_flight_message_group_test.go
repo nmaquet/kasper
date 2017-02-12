e7f3905de35b78ec0b87447ee8d137288426b562
Nicolas Maquet <nicolas@movio.co>
2017-02-13 09:55:31 +1300

    master: Add one test

--- in_flight_message_group_test.go
+++ in_flight_message_group_test.go
@@ -17,35 +17,44 @@ func TestInFlightMessageGroup_allAcksAreTrue_emptyGroup(t *testing.T) {
 
 func TestInFlightMessageGroup_allAcksAreTrue_noAcks(t *testing.T) {
 	g := inFlightMessageGroup{
-		incomingMessage:  &IncomingMessage{},
+		incomingMessage: &IncomingMessage{},
 		inFlightMessages: []*inFlightMessage{
 			{
 				msg: &sarama.ProducerMessage{},
 				ack: false,
 			},
 		},
-		committed:        false,
+		committed: false,
 	}
 	assert.False(t, g.allAcksAreTrue())
 }
 
-func TestInFlightMessageGroup_allAcksAreTrue_oneAck(t *testing.T) {
+func TestInFlightMessageGroup_allAcksAreTrue_nilGroup(t *testing.T) {
 	g := inFlightMessageGroup{
 		incomingMessage:  &IncomingMessage{},
+		inFlightMessages: nil,
+		committed:        false,
+	}
+	assert.True(t, g.allAcksAreTrue())
+}
+
+func TestInFlightMessageGroup_allAcksAreTrue_oneAck(t *testing.T) {
+	g := inFlightMessageGroup{
+		incomingMessage: &IncomingMessage{},
 		inFlightMessages: []*inFlightMessage{
 			{
 				msg: &sarama.ProducerMessage{},
 				ack: true,
 			},
 		},
-		committed:        false,
+		committed: false,
 	}
 	assert.True(t, g.allAcksAreTrue())
 }
 
 func TestInFlightMessageGroup_allAcksAreTrue_mixed(t *testing.T) {
 	g := inFlightMessageGroup{
-		incomingMessage:  &IncomingMessage{},
+		incomingMessage: &IncomingMessage{},
 		inFlightMessages: []*inFlightMessage{
 			{
 				msg: &sarama.ProducerMessage{},
@@ -56,7 +65,7 @@ func TestInFlightMessageGroup_allAcksAreTrue_mixed(t *testing.T) {
 				ack: false,
 			},
 		},
-		committed:        false,
+		committed: false,
 	}
 	assert.False(t, g.allAcksAreTrue())
 }
