26b45ef0091e4e6f380be2736d7faebd327f9822
Nicolas Maquet <nicolas@movio.co>
2017-02-13 09:53:08 +1300

    master: Add test for inFlightMessageGroup

---
+++ in_flight_message_group_test.go
@@ -0,0 +1,62 @@
+package kasper
+
+import (
+	"testing"
+	"github.com/Shopify/sarama"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestInFlightMessageGroup_allAcksAreTrue_emptyGroup(t *testing.T) {
+	g := inFlightMessageGroup{
+		incomingMessage:  &IncomingMessage{},
+		inFlightMessages: []*inFlightMessage{},
+		committed:        false,
+	}
+	assert.True(t, g.allAcksAreTrue())
+}
+
+func TestInFlightMessageGroup_allAcksAreTrue_noAcks(t *testing.T) {
+	g := inFlightMessageGroup{
+		incomingMessage:  &IncomingMessage{},
+		inFlightMessages: []*inFlightMessage{
+			{
+				msg: &sarama.ProducerMessage{},
+				ack: false,
+			},
+		},
+		committed:        false,
+	}
+	assert.False(t, g.allAcksAreTrue())
+}
+
+func TestInFlightMessageGroup_allAcksAreTrue_oneAck(t *testing.T) {
+	g := inFlightMessageGroup{
+		incomingMessage:  &IncomingMessage{},
+		inFlightMessages: []*inFlightMessage{
+			{
+				msg: &sarama.ProducerMessage{},
+				ack: true,
+			},
+		},
+		committed:        false,
+	}
+	assert.True(t, g.allAcksAreTrue())
+}
+
+func TestInFlightMessageGroup_allAcksAreTrue_mixed(t *testing.T) {
+	g := inFlightMessageGroup{
+		incomingMessage:  &IncomingMessage{},
+		inFlightMessages: []*inFlightMessage{
+			{
+				msg: &sarama.ProducerMessage{},
+				ack: true,
+			},
+			{
+				msg: &sarama.ProducerMessage{},
+				ack: false,
+			},
+		},
+		committed:        false,
+	}
+	assert.False(t, g.allAcksAreTrue())
+}
