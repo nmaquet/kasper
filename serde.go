1a7f6c48dc13c11579828c8b0c186185299ab777
Vlad Emelianov <vlad@movio.co>
2017-02-14 11:42:07 +1300

    typo

--- serde.go
+++ serde.go
@@ -2,8 +2,8 @@ package kasper
 
 // TopicSerde describes a serdes interface for keys and values of a Kafka topic
 type TopicSerde struct {
-	KeySerde   Serde // Srrde used for Kafka message keys
-	ValueSerde Serde // Srrde used for Kafka message values
+	KeySerde   Serde // Serde used for Kafka message keys
+	ValueSerde Serde // Serde used for Kafka message values
 }
 
 // Serde describes a serializer/deserializer interface
