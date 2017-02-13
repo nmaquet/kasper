7bc73421f320211a3ffc2af5f1165ba1b58b5090
Vlad Emelianov <vlad@movio.co>
2017-02-13 18:37:52 +1300

    gofmt one love

--- config_test.go
+++ config_test.go
@@ -2,14 +2,15 @@ package kasper
 
 import (
 	"testing"
-	"github.com/stretchr/testify/assert"
+
 	"github.com/Shopify/sarama"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestDefaultConfig(t *testing.T) {
 	actual := DefaultConfig()
 	expected := &Config{
-		RequiredAcks: sarama.WaitForAll,
+		RequiredAcks:             sarama.WaitForAll,
 		MaxInFlightMessageGroups: 5000,
 	}
 	assert.Equal(t, expected, actual)
