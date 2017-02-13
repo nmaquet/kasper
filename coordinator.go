714e13e0ef5193088024835afbeedbe8fcfbf785
Vlad Emelianov <vlad@movio.co>
2017-02-13 13:31:59 +1300

    comment for coordinator

--- coordinator.go
+++ coordinator.go
@@ -10,10 +10,12 @@ type partitionProcessorCoordinator struct {
 	pp *partitionProcessor
 }
 
+// Commit will commit the next in-flight message group
 func (c *partitionProcessorCoordinator) Commit() {
 	c.pp.commitNextInFlightMessageGroup = true
 }
 
+// ShutdownTopicProcessor safely shuts down topic processor, closing both client and producer
 func (c *partitionProcessorCoordinator) ShutdownTopicProcessor() {
 	c.pp.topicProcessor.onShutdown()
 }
