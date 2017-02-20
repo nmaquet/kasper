b449d5601bdf30f450cfe563037c5b701f8d0c4c
Vlad Emelianov <vlad@movio.co>
2017-02-21 11:51:49 +1300

    bugfixes for Redis KV

--- kv/redis_kv.go
+++ kv/redis_kv.go
@@ -1,26 +1,40 @@
 package kv
 
 import (
+	"encoding/json"
+
+	"fmt"
+
 	"github.com/garyburd/redigo/redis"
 	"github.com/movio/kasper/util"
-	"encoding/json"
 )
 
 type RedisKeyValueStore struct {
-	conn    redis.Conn
-	witness *util.StructPtrWitness
+	conn      redis.Conn
+	keyPrefix string
+	witness   *util.StructPtrWitness
 }
 
-func NewRedisKeyValueStore(url string, structPtr interface{}) (*RedisKeyValueStore, error) {
+func NewRedisKeyValueStore(url string, keyPrefix string, structPtr interface{}) (*RedisKeyValueStore, error) {
 	conn, err := redis.DialURL(url)
 	if err != nil {
 		return nil, err
 	}
-	return &RedisKeyValueStore{conn, util.NewStructPtrWitness(structPtr)}, nil
+	return &RedisKeyValueStore{conn, keyPrefix, util.NewStructPtrWitness(structPtr)}, nil
+}
+
+func (s *RedisKeyValueStore) getPrefixedKey(key string) string {
+	return fmt.Sprintf("%s%s", s.keyPrefix, key)
 }
 
 func (s *RedisKeyValueStore) Get(key string) (interface{}, error) {
-	response, err := s.conn.Do("GET", key)
+	response, err := s.conn.Do("GET", s.getPrefixedKey(key))
+	if err != nil {
+		return s.witness.Nil(), err
+	}
+	if response == nil {
+		return s.witness.Nil(), nil
+	}
 	bytes, err := redis.Bytes(response, err)
 	if err != nil {
 		return s.witness.Nil(), err
@@ -39,29 +53,33 @@ func (s *RedisKeyValueStore) Put(key string, structPtr interface{}) error {
 	if err != nil {
 		return err
 	}
-	_, err = s.conn.Do("PUT", key, bytes)
+	_, err = s.conn.Do("SET", s.getPrefixedKey(key), bytes)
 	return err
 }
 
 func (s *RedisKeyValueStore) PutAll(entries []*Entry) error {
-	s.conn.Send("MULTI")
+	err := s.conn.Send("MULTI")
+	if err != nil {
+		return err
+	}
 	for _, entry := range entries {
 		s.witness.Assert(entry.value)
-		bytes, err := json.Marshal(entry.value)
+		var bytes []byte
+		bytes, err = json.Marshal(entry.value)
 		if err != nil {
 			return err
 		}
-		err = s.conn.Send("PUT", entry.key, bytes)
+		err = s.conn.Send("SET", s.getPrefixedKey(entry.key), bytes)
 		if err != nil {
 			return err
 		}
 	}
-	_, err := s.conn.Do("EXEC")
+	_, err = s.conn.Do("EXEC")
 	return err
 }
 
 func (s *RedisKeyValueStore) Delete(key string) error {
-	_, err := s.conn.Do("DEL", key)
+	_, err := s.conn.Do("DEL", s.getPrefixedKey(key))
 	return err
 }
 
