cb8bda8ae7dddd11cdc1cbbec071b1e1c086cf22
Nicolas Maquet <nicolas@movio.co>
2017-02-24 12:33:49 +1300

    Remove unused config

--- config.go
+++ config.go
@@ -11,9 +11,6 @@ type Config struct {
 	// The level of acknowledgement reliability needed from the broker.
 	// Default: sarama.WaitForAll, wait for all in-sync replicas to commit.
 	RequiredAcks sarama.RequiredAcks
-	// Limit amount of message groups processed simultaneously.
-	// Default: 5000.
-	MaxInFlightMessageGroups int
 	// MarkOffsetsHook is called by Kasper prior to marking offsets when automatic offset marking is enabled.
 	// This hook can be used to synchronously flush output buffers or external storage before marking offsets.
 	// Default: a no-op funcMetrics
@@ -28,7 +25,6 @@ type Config struct {
 func DefaultConfig() *Config {
 	return &Config{
 		RequiredAcks:             sarama.WaitForAll,
-		MaxInFlightMessageGroups: 5000,
 		MarkOffsetsHook:          func() {},
 		MetricsProvider:          &NoopMetricsProvider{},
 		MetricsUpdateInterval:    5 * time.Second,
--- config_test.go
+++ config_test.go
@@ -11,5 +11,4 @@ func TestDefaultConfig(t *testing.T) {
 	config := DefaultConfig()
 	assert.NotNil(t, config.MarkOffsetsHook)
 	assert.Equal(t, sarama.WaitForAll, config.RequiredAcks)
-	assert.Equal(t, 5000, config.MaxInFlightMessageGroups)
 }
