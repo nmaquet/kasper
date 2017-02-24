1083661b9aa49a1d0f58ff5c50f452a40d62bcd1
Nicolas Maquet <nicolas@movio.co>
2017-02-25 11:50:37 +1300

    Fix create topics script

--- ci/create_topics.sh
+++ ci/create_topics.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-docker exec -ti kafka_kafka_1 /opt/kafka_2.11-0.10.0.1/bin/kafka-topics.sh --zookeeper 172.18.0.1:2181 --create --topic characters --replication-factor 1 --partitions 1
-docker exec -ti kafka_kafka_1 /opt/kafka_2.11-0.10.0.1/bin/kafka-topics.sh --zookeeper 172.18.0.1:2181 --create --topic fictions --replication-factor 1 --partitions 1
-docker exec -ti kafka_kafka_1 /opt/kafka_2.11-0.10.0.1/bin/kafka-topics.sh --zookeeper 172.18.0.1:2181 --create --topic fictions-and-characters --replication-factor 1 --partitions 1
+docker exec -ti ci_kafka_1 /opt/kafka_2.11-0.10.0.1/bin/kafka-topics.sh --zookeeper 172.18.0.1:2181 --create --topic characters --replication-factor 1 --partitions 1
+docker exec -ti ci_kafka_1 /opt/kafka_2.11-0.10.0.1/bin/kafka-topics.sh --zookeeper 172.18.0.1:2181 --create --topic fictions --replication-factor 1 --partitions 1
+docker exec -ti ci_kafka_1 /opt/kafka_2.11-0.10.0.1/bin/kafka-topics.sh --zookeeper 172.18.0.1:2181 --create --topic fictions-and-characters --replication-factor 1 --partitions 1
