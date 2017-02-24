8f7a8a15ed951b49e4782e7d7d189f6962b3ba84
Nicolas Maquet <nicolas@movio.co>
2017-02-25 12:33:34 +1300

    Add tests for prometheus metrics provider

---
+++ prometheus_metrics_test.go
@@ -0,0 +1,15 @@
+package kasper
+
+import "testing"
+
+func TestNewPrometheusMetricsProvider(t *testing.T) {
+	provider := NewPrometheusMetricsProvider("test", 0)
+	gauge := provider.NewGauge("test_gauge", "A test gauge", "label1", "label2")
+	counter := provider.NewCounter("test_counter", "A test counter", "label1", "label2")
+	summary := provider.NewSummary("test_summary", "A test summary", "label1", "label2")
+
+	gauge.Set(42, "value1", "value2")
+	counter.Inc("value1", "value2")
+	counter.Add(1, "value1", "value2")
+	summary.Observe(42, "value1", "value2")
+}
