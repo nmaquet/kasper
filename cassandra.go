2a5e0f74b31ce9ebec6eb08bd10ba367dd62283b
Nicolas Maquet <nicolas@movio.co>
2017-04-28 00:00:49 +1200

    basic cassandra implementation done

--- cassandra.go
+++ cassandra.go
@@ -1,23 +1,20 @@
 package kasper
 
 import (
-	"fmt"
 	cassandra "github.com/gocql/gocql"
-	"github.com/pkg/errors"
 )
 
-type CassandraMapping struct {
-	keySpace        string
-	tableName       string
-	primaryKeyName  string
-	valueColumnName string
+type CassandraQueries interface {
+	Select(keys []string) (string, []interface{})
+	Insert(key string, value []byte) (string, []interface{})
+	Delete(key string) (string, []interface{})
 }
 
 // Cassandra is an implementation of Store that uses Cassandra.
 // FIXME: add doc
 type Cassandra struct {
 	session       *cassandra.Session
-	mapping       CassandraMapping
+	queries       CassandraQueries
 	logger        Logger
 	labelValues   []string
 	getCounter    Counter
@@ -30,7 +27,7 @@ type Cassandra struct {
 
 // NewCassandra creates Cassandra instances. All keys read and written in Cassandra are of the form:
 // FIXME: add doc
-func NewCassandra(config *Config, session *cassandra.Session, mapping CassandraMapping) *Cassandra {
+func NewCassandra(config *Config, session *cassandra.Session, mapping CassandraQueries) *Cassandra {
 	metrics := config.MetricsProvider
 	labelNames := []string{"topicProcessor"}
 	return &Cassandra{
@@ -48,49 +45,56 @@ func NewCassandra(config *Config, session *cassandra.Session, mapping CassandraM
 }
 
 func (c *Cassandra) Get(key string) ([]byte, error) {
-	fmtStr := "SELECT \"%s\" FROM \"%s\".\"%s\" WHERE \"%s\" = ?"
-	queryStr := fmt.Sprintf(
-		fmtStr,
-		c.mapping.valueColumnName,
-		c.mapping.keySpace,
-		c.mapping.tableName,
-		c.mapping.primaryKeyName,
-	)
-	iter := c.session.Query(queryStr, key).Iter()
+	statement, bindings := c.queries.Select([]string{key})
+	var value []byte
+	iter := c.session.Query(statement, bindings...).Iter()
 	if iter.NumRows() == 0 {
-		return nil, nil
+		return nil, iter.Close()
 	}
-	var data []byte
-	if !iter.Scan(&data) {
-		return nil, errors.New("Failed to scan Cassandra value")
+	iter.Scan(&key, &value)
+	if err := iter.Close(); err != nil {
+		return nil, err
 	}
-	return data, nil
+	return value, nil
 }
 
 func (c *Cassandra) GetAll(keys []string) (map[string][]byte, error) {
-	panic("not implemented")
+	statement, bindings := c.queries.Select(keys)
+	iter := c.session.Query(statement, bindings...).Iter()
+	result := make(map[string][]byte, len(keys))
+	var key string
+	var value []byte
+	for iter.Scan(&key, &value) {
+		result[key] = value
+	}
+	err := iter.Close()
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
 }
 
 func (c *Cassandra) Put(key string, value []byte) error {
-	fmtStr := "INSERT INTO \"%s\".\"%s\" (\"%s\", \"%s\") VALUES (?, ?)"
-	queryStr := fmt.Sprintf(fmtStr,
-		c.mapping.keySpace,
-		c.mapping.tableName,
-		c.mapping.primaryKeyName,
-		c.mapping.valueColumnName,
-	)
-	query := c.session.Query(queryStr, key, value)
+	statement, bindings := c.queries.Insert(key, value)
+	query := c.session.Query(statement, bindings...)
 	return query.Exec()
 }
 
 func (c *Cassandra) PutAll(entries map[string][]byte) error {
-	panic("not implemented")
+	batch := c.session.NewBatch(cassandra.LoggedBatch)
+	for key, value := range entries {
+		statement, bindings := c.queries.Insert(key, value)
+		batch.Query(statement, bindings...)
+	}
+	return c.session.ExecuteBatch(batch)
 }
 
 func (c *Cassandra) Delete(key string) error {
-	panic("not implemented")
+	statement, bindings := c.queries.Delete(key)
+	query := c.session.Query(statement, bindings...)
+	return query.Exec()
 }
 
 func (c *Cassandra) Flush() error {
-	panic("not implemented")
+	panic("Cassandra.Flush() is not supported.")
 }
--- cassandra_test.go
+++ cassandra_test.go
@@ -1,8 +1,10 @@
 package kasper
 
 import (
+	"fmt"
 	cassandra "github.com/gocql/gocql"
 	"github.com/stretchr/testify/assert"
+	"strings"
 	"testing"
 	"time"
 )
@@ -25,6 +27,97 @@ func TestCassandra_Get_Put(t *testing.T) {
 	// Get key again, should find it this time
 	dragon, err := cassandraStore.Get("vorgansharax")
 	assert.Equal(t, vorgansharax, dragon)
+
+	// Delete key
+	err = cassandraStore.Delete("vorgansharax")
+	assert.Nil(t, err)
+
+	// Get key again, should not find it anymore
+	item, err = cassandraStore.Get("vorgansharax")
+	assert.Nil(t, item)
+	assert.Nil(t, err)
+}
+
+func TestCassandra_Delete(t *testing.T) {
+	if testing.Short() {
+		t.Skip()
+	}
+	// Delete non-existing key
+	err := cassandraStore.Delete("saphira")
+	assert.Nil(t, err)
+
+	// Put key
+	err = cassandraStore.Put("saphira", saphira)
+	assert.Nil(t, err)
+
+	// Delete key again, should work this time
+	err = cassandraStore.Delete("saphira")
+	assert.Nil(t, err)
+
+	// Get key again, should not find it anymore
+	dragon, err := cassandraStore.Get("saphira")
+	assert.Nil(t, dragon)
+	assert.Nil(t, err)
+}
+
+func TestCassandra_GetAll(t *testing.T) {
+	if testing.Short() {
+		t.Skip()
+	}
+	err := cassandraStore.Put("vorgansharax", vorgansharax)
+	assert.Nil(t, err)
+
+	err = cassandraStore.Put("saphira", saphira)
+	assert.Nil(t, err)
+
+	err = cassandraStore.Put("falkor", falkor)
+	assert.Nil(t, err)
+
+	kvs, err := cassandraStore.GetAll([]string{"vorgansharax", "saphira", "batman"})
+	assert.Nil(t, err)
+	assert.Equal(t, 2, len(kvs))
+	assert.Equal(t, vorgansharax, kvs["vorgansharax"])
+	assert.Equal(t, saphira, kvs["saphira"])
+}
+
+func TestCassandra_PutAll(t *testing.T) {
+	if testing.Short() {
+		t.Skip()
+	}
+	err := cassandraStore.PutAll(map[string][]byte{
+		"vorgansharax": vorgansharax,
+		"saphira":      saphira,
+		"falkor":       falkor,
+	})
+	assert.Nil(t, err)
+
+	kvs, err := cassandraStore.GetAll([]string{"vorgansharax", "saphira", "batman"})
+	assert.Nil(t, err)
+	assert.Equal(t, 2, len(kvs))
+	assert.Equal(t, vorgansharax, kvs["vorgansharax"])
+	assert.Equal(t, saphira, kvs["saphira"])
+}
+
+type testMapping struct{}
+
+func (testMapping) Select(keys []string) (string, []interface{}) {
+	qMarks := make([]string, len(keys))
+	bindings := make([]interface{}, len(keys))
+	for i := range keys {
+		qMarks[i] = "?"
+		bindings[i] = keys[i]
+	}
+	format := "SELECT key, value FROM kasper.test WHERE key in (%s)"
+	statement := fmt.Sprintf(format, strings.Join(qMarks, ", "))
+	return statement, bindings
+}
+
+func (testMapping) Insert(key string, value []byte) (string, []interface{}) {
+	return "INSERT INTO kasper.test (key, value) VALUES (?, ?)", []interface{}{key, value}
+}
+
+func (testMapping) Delete(key string) (string, []interface{}) {
+	return "DELETE FROM kasper.test WHERE key = ?", []interface{}{key}
 }
 
 func init() {
@@ -33,12 +126,12 @@ func init() {
 	}
 	config := &Config{
 		TopicProcessorName: "test",
-		Logger:             &noopLogger{},
+		Logger:             NewBasicLogger(true),
 		MetricsProvider:    &noopMetricsProvider{},
 	}
 	cluster := cassandra.NewCluster(getCIHost())
 	cluster.Consistency = cassandra.LocalOne
-	cluster.Timeout = time.Second * 5
+	cluster.Timeout = time.Second * 10
 	cluster.ProtoVersion = 4
 	session, err := cluster.CreateSession()
 	if err != nil {
@@ -56,10 +149,5 @@ func init() {
 	if err != nil {
 		panic(err)
 	}
-	cassandraStore = NewCassandra(config, session, CassandraMapping{
-		keySpace:        "kasper",
-		tableName:       "test",
-		primaryKeyName:  "key",
-		valueColumnName: "value",
-	})
+	cassandraStore = NewCassandra(config, session, testMapping{})
 }
